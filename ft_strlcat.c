e277f8cb9951b28cb4a34455faa975f48fdaa33e
irene <wunder.frau@yandex.by>
2023-11-07 07:52:22 +0200

    fix: errors ft_strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -19,13 +19,13 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 	size_t	dst_len;
 	size_t	src_len;
 
-	if (dstsize == 0)
-		return (0);
+	if (!dstsize)
+		return (ft_strlen(src));
 	src_len = ft_strlen((char *)src);
 	dst_len = ft_strlen(dst);
 	j = dst_len;
 	i = 0;
-	if (dst_len < dstsize - 1 && dstsize > 0)
+	if (dst_len < dstsize - 1 || dstsize > 0)
 	{
 		while (src[i] && dst_len + i < dstsize - 1)
 		{
