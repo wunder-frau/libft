3df01e78720db3fe124bd17218ed3e7e65cf7595
Iryna Stasheuski <istasheu@c3r3p6.hive.fi>
2023-11-09 16:23:33 +0200

    fix: updated malloc value with sizeof(*t_list) instead of sizeof(*new_node)

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: istasheu <istasheu@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 11:35:34 by istasheu          #+#    #+#             */
-/*   Updated: 2023/11/08 11:46:35 by istasheu         ###   ########.fr       */
+/*   Updated: 2023/11/09 10:23:19 by istasheu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ t_list	*ft_lstnew(void *content)
 {
 	t_list	*new_node;
 
-	new_node = (t_list *)malloc(sizeof(*new_node));
+	new_node = (t_list *)malloc(sizeof(t_list));
 	if (!new_node)
 		return (NULL);
 	new_node->content = content;
