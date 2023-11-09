16e0f20ccdbf58cb59e9e92d0fe4a5a0adfd399c
irene <wunder.frau@yandex.by>
2023-11-09 07:25:10 +0200

    develop bonus ft_lstclear

---
+++ ft_lstclear.c
@@ -0,0 +1,16 @@
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list *temp;
+
+	if (lst)
+	{
+		while (*lst) 
+		{
+			temp = (*lst)->next;
+			ft_lstdelone(*lst, del);
+			(*lst) = temp;
+		}
+	}
+}
