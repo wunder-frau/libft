035ac4f5cef45fe4e7329afba35856d4fa6a649d
Iryna Stasheuski <istasheu@c3r1p7.hive.fi>
2023-11-15 13:12:59 +0200

    fix: ft_atoi now resilves overflow

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,39 +6,42 @@
 /*   By: istasheu <istasheu@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 08:41:52 by istasheu          #+#    #+#             */
-/*   Updated: 2023/11/14 17:42:07 by istasheu         ###   ########.fr       */
+/*   Updated: 2023/11/15 12:56:03 by istasheu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include "limits.h"
 
 int	ft_atoi(const char *str)
 {
-	int				i;
 	int				is_negative;
-	long long int	number;
+	long long		number;
 
-	i = 0;
 	is_negative = 1;
 	number = 0;
-	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
-		i++;
-	if (str[i] == '+' || str[i] == '-')
+	while ((*str >= 9 && *str <= 13) || *str == 32)
+		str++;
+	if (*str == '-' || *str == '+')
 	{
-		if (str[i] == '-')
+		if (*(str++) == '-')
 			is_negative = -1;
-		i++;
 	}
-	while (str[i] >= 48 && str[i] <= 57)
+	while (*str >= 48 && *str <= 57)
 	{
-		number *= 10;
-		number += (str[i] - 48);
-		i++;
+		if (number >= LONG_MAX / 10 && (number > LONG_MAX / 10 \
+					|| (*str - '0') > LONG_MAX % 10))
+		{
+			if (is_negative == 1)
+				return (-1);
+			else if (is_negative == -1)
+				return (0);
+		}
+		number = number * 10 + (*(str++) - '0');
 	}
-	return (number * is_negative);
+	return ((int)number * is_negative);
 }
-
-#include <stdlib.h>
+/*#include <stdlib.h>
 #include <stdio.h>
 #include <libc.h>
 int main(void)
@@ -58,4 +61,4 @@ int main(void)
         free(str);
         i++;
     }
-}
+}*/
