661cdc43bbc3d2cd5e24b047f5c09526442db5f5
Iryna Stasheuski <istasheu@c3r1p7.hive.fi>
2023-11-15 13:10:13 +0200

    fix: ft_callol total is -1

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,31 +6,20 @@
 /*   By: istasheu <istasheu@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/01 14:13:09 by istasheu          #+#    #+#             */
-/*   Updated: 2023/11/14 10:02:34 by istasheu         ###   ########.fr       */
+/*   Updated: 2023/11/15 13:01:17 by istasheu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-static void *ft_overflow(size_t count, size_t size)
-{
-	if (count == 0 || size == 0)
-	{
-		return (NULL);
-	}
-	if (size > SIZE_MAX / count)
-	{
-		return (NULL);
-	}
-	return ((void *)1);
-}
 
 void	*ft_calloc(size_t count, size_t size)
 {
 	void			*dest;
 	size_t			total;
 
-	ft_overflow(count, size);
+	total = -1;
+	if (count != 0 && total / count < size)
+		return (NULL);
 	total = count * size;
 	dest = malloc(total);
 	if (dest == NULL)
